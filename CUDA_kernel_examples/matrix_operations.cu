1d26335a19684befdca52fe202341e27e42d907b
Aly <alyshariff2810@gmail.com>
2025-11-16 21:37:36 -0500

    feat: naive matmul kernel

--- CUDA_kernel_examples/matrix_operations.cu
+++ CUDA_kernel_examples/matrix_operations.cu
@@ -21,12 +21,62 @@ __global__ void matrix_add(int* A, int* B, int* out, int N_x, int N_y){
 
 
 // Exercise 2
-__global__ void matrix_multiply(int* A, int* B, int* out, int N){
-
+__global__ void matrix_multiply(int* A, int* B, int* out, int N_A_x, int N_A_y, int N_B_x, int N_B_y){
+    // Naive implementation: Each thread is responsible for one (i, j) entry in out (stored in row-major)
+    int column = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    if (column < N_B_x && row < N_A_y){
+        int sum = 0; // Prevent multiple writes to GPU
+        for (int i = 0; i < N_A_x; ++i){
+            sum += A[row * N_A_x + i] * B[i * N_B_x + column];
+        }
+        out[row * N_B_x + column] = 0;
+    }
 }
 
 
-// BONUS: Tiled matmul with shared memory
+// BONUS (later): Tiled matmul with shared memory
+
+
+void test_matrix_multiply(int N_A_x, int N_A_y, int N_B_x, int N_B_y, int block_size_x, int block_size_y){
+    int *d_A, *d_B, *d_C;
+    int *h_A = malloc(sizeof(int) * N_A_x * N_A_y);
+    int *h_B = malloc(sizeof(int) * N_B_x * N_B_y);
+    int *h_C = malloc(sizeof(int) * N_B_x * N_A_y);
+
+    for (int i = 0; i < N_A_x * N_A_y; ++i){
+        h_A[i] = rand() % 100;
+    }
+
+    for (int i = 0; i < N_B_x * N_B_y; ++i){
+        h_B[i] = rand() % 100;
+    }
+
+    int N_A = N_A_x * N_A_y;
+    int N_B = N_B_x * N_B_y;
+    int N_C = N_C_x * N_C_y;
+    cudaMalloc((void**)&d_A, sizeof(int) * N_A);
+    cudaMalloc((void**)&d_B, sizeof(int) * N_B);
+    cudaMalloc((void**)&d_C, sizeof(int) * N_C);
+    cudaMemcpy(d_A, h_A, sizeof(int) * N_A, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, sizeof(int) * N_B, cudaMemcpyHostToDevice);
+    
+
+    dim3 grid_size((N_C_x + block_size_x - 1) / block_size_x, (N_C_y + block_size_y - 1) / block_size_y, 1);
+    dim3 block_size(block_size_x, block_size_y, 1)
+
+    matrix_add<<grid_size, block_size>>(d_A, d_B, d_out, N_C_x, N_C_y);
+    cudaMemcpy(h_C, d_C, sizeof(int) * N_C, cudaMemcpyDeviceToHost);
+
+    // Deallocate
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    free(h_A);
+    free(h_B);
+    free(h_C);
+}
 
 
 
@@ -57,11 +107,11 @@ void test_matrix_add(int N_x, int N_y, int block_size_x, int block_size_y){
     // Deallocate
     cudaFree(d_A);
     cudaFree(d_B);
-    cudaFree(d_out);
+    cudaFree(d_C);
 
     free(h_A);
     free(h_B);
-    free(h_out);
+    free(h_C);
 }
 
 void test_vector_add(int N, int block_size){
