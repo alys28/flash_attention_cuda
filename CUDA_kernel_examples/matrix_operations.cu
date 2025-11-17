6d3a820e38e824583f2413104034ef30d700d07d
Aly <alyshariff2810@gmail.com>
2025-11-16 20:21:30 -0500

    feat: basic vector addition

--- CUDA_kernel_examples/matrix_operations.cu
+++ CUDA_kernel_examples/matrix_operations.cu
@@ -1,5 +1,6 @@
 
 #include "cuda_common.cuh"
+#include <math.h>
 
 // Simple addition kernel
 __global__ vector_add(int* A, int* B, int* out, int N){
@@ -26,10 +27,44 @@ __global__ matrix_multiply(int* A, int* B, int* out, int N){
 
 
 void test_add(int N, int block_size){
-    int *A, *B, *out;
+    int *d_A, *d_B, *d_out;
+    int *h_A, *h_B, *h_out; 
     // Allocate on host (CPU)
-    A = malloc(sizeof(int), N);
-    B = malloc(sizeof(int), N);
+    h_A = malloc(sizeof(int) * N);
+    h_B = malloc(sizeof(int) * N);
+    h_out = malloc(sizeof(int) * N);
+    // Initialize arrays
+    for (int i = 0; i < N; ++i){
+        h_A[i] = 1;
+        h_B[i] = 10;
+    }
+    // Allocate on device (GPU)
+    cudaMalloc((void**)&d_A, sizeof(int) * N);
+    cudaMalloc((void**)&d_B, sizeof(int) * N);
+    cudaMalloc((void**)&d_C, sizeof(int) * N);
+    cudaMemcpy(d_A, h_A, sizeof(int) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, sizeof(int) * N, cudaMemcpyHostToDevice);
+
+    // Execute kernel on device
+    int grid_size = (N + block_size - 1) / block_size;
+    vector_add<<grid_size, block_size>>(d_A, d_B, d_out, N);
+    // Get result on host
+    cudaMemcpy(h_C, d_C, sizeof(int) * N, cudaMemcpyDeviceToHost);
+    
+
+    // Deallocate
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+
+    free(A);
+    free(B);
+    free(out);
 }
 
+
+
+int main(){
+    test_add(40, 3);
+}
 
